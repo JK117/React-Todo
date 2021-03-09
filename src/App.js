af209b6b45a50c81a73b5d577ae7a87431c04401
JK117 <sheppard.jk@outlook.com>
2021-03-09 21:38:01 +0800

    Test Product component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 
 import Joke from './components/Joke'
 import jokesData from './data/jokesData'
+import Product from './components/Product'
+import productData from './data/productsData'
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
@@ -14,8 +16,11 @@ import jokesData from './data/jokesData'
 
 function App() {
     const jokesComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} answer={joke.punchLine} />)
+    const productComponents = productData.map(item => <Product key={item.id} product={item} />)
     return (
         <div>
+            {productComponents}
+            <hr/>
             {jokesComponents}
         </div>
     )
